c79e8b4386bb55c4d619c36e70e97cc0f3992df0
unknown <luchisgapo@gmail.com>
2023-05-17 18:33:47 -0500

    start

---
+++ errore/errore.js
@@ -0,0 +1,20 @@
+var nav = document.querySelector("nav");
+document.querySelector(".boton").addEventListener("click", function() {
+    nav.classList.toggle("active");
+});
+
+
+
+
+let nav = document.querySelector("nav"); document.querySelector(".boton").addEventListener("click", function(){ nav.classList.toggle("active") });
+
+document.querySelector(".boton").addEventListener("click", function() {
+    document.querySelector("nav ul").classList.toggle("active");
+});
+
+
+
+
+
+
+/*css*/
